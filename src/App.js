b5780014a7015b79c08c4409ca8bd677c0ef3dee
Daniel Rigberg <daniel.rigberg@gmail.com>
2017-11-17 18:00:57 +0100

    use react with electron

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ class App extends Component {
   render() {
     return (
       <div id="app">
-        <h1>Google Analytics 2 Sheets</h1>
+        <h1>Google Analytics 4 Sheets</h1>
 
         <div>
           <div>
             <h3>Metrics</h3>
 
             <input className="metrics" id="sessions" type="checkbox"></input>
-            <label for="sessions">Sessions</label>
+            <label htmlFor="sessions">Sessions</label>
 
             <input className="metrics" id="pageLoadTime" type="checkbox"></input>
-            <label for="pageLoadTime">Page Load Time</label>
+            <label htmlFor="pageLoadTime">Page Load Time</label>
 
             <input className="metrics" id="timeOnPage" type="checkbox"></input>
-            <label for="timeOnpage">Time On Page</label>
+            <label htmlFor="timeOnpage">Time On Page</label>
 
           </div>
 
@@ -27,22 +27,22 @@ class App extends Component {
             <h3>Dimensions</h3>
 
             <input className="dimensions" id="screenResolution" type="checkbox"></input>
-            <label for="screenResolution">Screen resolution</label>
+            <label htmlFor="screenResolution">Screen resolution</label>
 
             <input className="dimensions" id="city" type="checkbox"></input>
-            <label for="city">City</label>
+            <label htmlFor="city">City</label>
 
             <input className="dimensions" id="country" type="checkbox"></input>
-            <label for="country">Country</label>
+            <label htmlFor="country">Country</label>
 
             <input className="dimensions" id="browser" type="checkbox"></input>
-            <label for="browser">Browser</label>
+            <label htmlFor="browser">Browser</label>
 
             <input className="dimensions" id="operatingSystem" type="checkbox"></input>
-            <label for="operatingSystem">Operating System</label>
+            <label htmlFor="operatingSystem">Operating System</label>
 
             <input className="dimensions" id="userType" type="checkbox"></input>
-            <label for="userType">User Type</label>
+            <label htmlFor="userType">User Type</label>
           </div>
         </div>
 
